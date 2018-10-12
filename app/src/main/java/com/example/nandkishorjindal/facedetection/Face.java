10d39e31c9d76864777ebdb055e25f313f56b64c
jindalshivani <sjindal807@gmail.com>
2018-10-12 14:25:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nandkishorjindal/facedetection/Face.java
@@ -0,0 +1,13 @@
+package com.example.nandkishorjindal.facedetection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Face extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face);
+    }
+}
